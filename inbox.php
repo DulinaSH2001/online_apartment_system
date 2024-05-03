e433a780ace37cff724225d66497fa1e527936e3
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-05-03 20:56:40 +0530

    chore: Refactor inbox.php to improve code readability and remove unnecessary comments

--- inbox.php
+++ inbox.php
@@ -17,24 +17,24 @@
 
         include 'connect.php';
 
-        // Check if the user is logged in (i.e., if the session variable is set)
+
         if (!isset($_SESSION['user'])) {
-            // If the user is not logged in, redirect them to the login page
+
             header("Location: login.php");
             exit();
         } else {
             echo "<h2>Welcome " . $_SESSION['user']['name'] . "</h2>";
 
-            // Get the user's email from the session
+
             $user_id = $_SESSION['user']['id'];
 
-            // Retrieve all complaints for the logged-in user from the database
+
             $query = "SELECT * FROM complaint WHERE u_id = '$user_id'";
             $result = mysqli_query($connect, $query);
 
-            // Check if there are any complaints for the logged-in user
+
             if (mysqli_num_rows($result) > 0) {
-                // Display each complaint and its corresponding reply (if any)
+
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo "<div class='complaint'>";
                     echo "<p>Email: " . $row['u_email'] . "</p>";
@@ -47,11 +47,11 @@
                     echo "</div>";
                 }
             } else {
-                // If no complaints are found for the logged-in user, display a message
+
                 echo "<p>No complaints found for this user.</p>";
             }
 
-            // Close the database connection
+
             mysqli_close($connect);
         }
         ?>
